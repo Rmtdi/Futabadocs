1b9be6f8fff7e6dbd0adb7dbefa70d3bbf02291f
Rmtdi <rmtdiyl@outlook.com>
2022-08-18 15:38:26 +0800

    注释掉搜索插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 const { defaultTheme } = require('@vuepress/theme-default')
-const { searchPlugin } = require('@vuepress/plugin-search')
+// const { searchPlugin } = require('@vuepress/plugin-search')
 
 module.exports = {
 //    dest: ".vuepress/dist",
@@ -9,7 +9,7 @@ module.exports = {
 
     description: '欢迎来到为双叶理央而创建的文档网站',
 
-    plugins: [
+/*    plugins: [
       searchPlugin({
         maxSuggestions: '15',
 	locales: {
@@ -19,7 +19,7 @@ module.exports = {
 	},
 	isSearchable: (page) => page.path !== '/',
       }),
-    ],
+    ],*/
 
     theme: defaultTheme({
        
